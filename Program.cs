2ca17a78a6d4f7a31909dbcda297bcc13f109e68
ignj <ignacio.jocano@gmail.com>
2023-11-19 15:13:59 +0100

    patch put method

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ app.MapHealthChecks("/healthcheck");
 app.MapGet("/products", async (IMediator mediator) => await mediator.Send(new GetProductListQuery())); //TODO: use outbound dto
 app.MapGet("/products/{id}", async (IMediator mediator, [FromRoute] int id) => await mediator.Send(new GetProductByIdQuery { Id = id }));
 app.MapPost("/products", async (IMediator mediator, [FromBody] CreateProductInboundDto dto) => await mediator.Send(new CreateProductCommand { Name = dto.Name, Stock = dto.Stock, Type = dto.Type, WarehouseName = dto.WarehouseName }));
-app.MapPut("/products/", async (IMediator mediator, [FromBody] UpdateProductInboundDto dto) => await mediator.Send(new UpdateProductCommand { Name = dto.Name, Stock = dto.Stock, Type = dto.Type, WarehouseName = dto.WarehouseName }));
+app.MapPut("/products/{id}", async (IMediator mediator, [FromRoute] int id, [FromBody] UpdateProductInboundDto dto) => await mediator.Send(new UpdateProductCommand { Id = id, Name = dto.Name, Stock = dto.Stock, Type = dto.Type, WarehouseName = dto.WarehouseName }));
 app.MapDelete("/products/{id}", async (IMediator mediator, [FromRoute] int id) => await mediator.Send(new DeleteProductCommand { Id = id }));
 
 app.Run();
